86dc94158988244873356a08dda847bdea2c3412
petrinidias <petrini4u@gmail.com>
2025-07-06 22:32:18 -0300

    gushi4

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-// Este arquivo será tratado como um Módulo ES devido ao "type": "module" no package.json
-export default {
-  plugins: {
-    tailwindcss: {}, // Sem 'require' aqui, o PostCSS resolve o plugin pelo nome
-    autoprefixer: {}, // Sem 'require' aqui
-  },
-};
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [
-    react() // Mantenha o plugin do React
+    react(),
+    tailwindcss()
   ],
-  // Não precisa do bloco 'css.postcss' aqui se você estiver usando um postcss.config.js
-});
+  
+})
