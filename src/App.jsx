b147d4573e2e4f4f6ef2b3abb8f022fa0ae1dff7
petrinidias <petrini4u@gmail.com>
2025-07-09 22:13:15 -0300

    gushi22

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Atendimento from "./Atendimento"
+import Atendimento from "./atendimento"
 import Ldpage from "./ldpage"
 
 function App() {
