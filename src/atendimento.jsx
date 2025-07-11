fe583b11f9c6235ffa2da60b394579585c4c82f0
petrinidias <petrini4u@gmail.com>
2025-07-10 22:00:08 -0300

    jradv37

--- src/atendimento.jsx
+++ src/atendimento.jsx
@@ -25,7 +25,7 @@ function Atendimento() {
     <div class="bg-white h-fit flex flex-col justify-center items-center py-[30px]">
       <p class="w-[90%] text-black font-bold text-[20px] text-center mb-[30px]">Olá {nome}, Clique no assunto para o qual deseja atendimento:</p>
       <a onClick={() => CriaLink(", fui vítima de um golpe do PIX e preciso de seu atendimento.")}
-        class="p-4 w-[90%] md:w-[690px] h-fit py-[20px]  bg-[#3148D2] text-[19px] flex flex-row text-center items-center justify-center font-medium text-white rounded-3xl cursor-pointer mt-[30px]  ">
+        class="p-4 w-[90%] md:w-[690px] h-fit py-[20px]  bg-[#3148D2] text-[19px] flex flex-row text-center items-center justify-center font-medium text-white rounded-3xl cursor-pointer">
         Fui vítima de um golpe do PIX
       </a>
       <a onClick={() => CriaLink(", tive uma negativação indevida e preciso de seu atendimento.")} class="p-4 w-[90%] md:w-[690px] h-fit py-[20px]  bg-[#3148D2] text-[19px] flex flex-row text-center items-center justify-center font-medium text-white rounded-3xl cursor-pointer mt-[30px]  ">
