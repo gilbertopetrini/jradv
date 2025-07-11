48df658f33d2718dd286e9599f00d577bc9f3149
petrinidias <petrini4u@gmail.com>
2025-07-10 21:20:00 -0300

    jradv33

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Atendimento from "./atendimento"
 import Ldpage from "./ldpage"
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Ldpage />} />
         <Route path="/atendimento" element={<Atendimento />} />
